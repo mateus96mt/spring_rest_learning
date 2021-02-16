ba7ffbce9aaa188b8492f4a19f42acb4b990a60c
Mateus Teixeira Magalhães <mateus.teixeira@haizen.tech>
2021-02-16 16:21:00 -0300

    mudança endpoint categorias

--- cursomc/src/main/java/com/mateus/cursomc/resources/CategoriaResource.java
+++ cursomc/src/main/java/com/mateus/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,29 @@
 package com.mateus.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.mateus.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "categorias";
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1, "informática");
+		Categoria cat2 = new Categoria(2, "escritório");
+		
+		List<Categoria> lista = new ArrayList<Categoria>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
+		
 	}
 
 }
