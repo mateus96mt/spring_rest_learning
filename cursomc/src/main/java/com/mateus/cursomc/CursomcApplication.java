0fde4b7bc305dcdf4e0d5ddf389a6961dd4c9afb
Mateus Teixeira Magalhães <mateus.teixeira@haizen.tech>
2021-02-16 17:38:14 -0300

    Criando operação para instanciação de objetos no banco: Categorias

--- cursomc/src/main/java/com/mateus/cursomc/CursomcApplication.java
+++ cursomc/src/main/java/com/mateus/cursomc/CursomcApplication.java
@@ -1,13 +1,38 @@
 package com.mateus.cursomc;
 
+import java.util.List;
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.mateus.cursomc.domain.Categoria;
+import com.mateus.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		List<Categoria> categorias = new ArrayList<Categoria>();
+		
+		categorias.add(new Categoria(null, "Informática"));
+		categorias.add(new Categoria(null, "Escritório"));
+		
+		categoriaRepository.saveAll(categorias);
+	}
+
+	
+	
 }
