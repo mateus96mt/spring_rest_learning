e40fdb4d3b20a623f7b2b8f3c6fe395037b8ef5c
Mateus Teixeira Magalhães <mateus.teixeira@haizen.tech>
2021-02-17 01:02:49 -0300

    Cricação de Cliente e Endereço - PARTE 1

--- cursomc/src/main/java/com/mateus/cursomc/domain/Estado.java
+++ cursomc/src/main/java/com/mateus/cursomc/domain/Estado.java
@@ -57,6 +57,31 @@ public class Estado implements Serializable{
 	public void setCidades(List<Cidade> cidades) {
 		this.cidades = cidades;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Estado other = (Estado) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
 	
 	
 	
